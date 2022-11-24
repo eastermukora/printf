25a9ff489af7ded49072b66078d03704dd03be80
leslie ochieng <105386873+leslie-katua-ochieng@users.noreply.github.com>
2022-11-24 11:51:47 +0300

    tasks

--- _putchar.c
+++ _putchar.c
@@ -1,1 +1,10 @@
-.
+#include "main.h"
+/**
+ * _putchar - prints one single character
+ * @c: char to print
+ * Return: integer with the amount of bytes printed
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
